644666e814d4e6b05b10deb76e1b9bcd3706e0d3
Matthew-swin <103219809@swin.edu.au>
2020-11-17 14:02:59 +1100

    funtimes

---
+++ DB_Rockie/DB_Rockie/Game.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Game]
+(
+	[GameStart] DATETIME NOT NULL, 
+    [userName] NVARCHAR(50) NOT NULL,
+    [playerChoice] NCHAR(10) NULL,
+    [cpuChoice] NCHAR(10) NULL, 
+    [round] INT NULL,
+    [roundResult] NCHAR(10) NULL,
+    PRIMARY KEY (GameStart, userName),
+    FOREIGN KEY (userName) REFERENCES Player 
+)
---
+++ DB_Rockie/DB_Rockie/Player.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Player]
+(
+	[userName] NVARCHAR(50) NOT NULL PRIMARY KEY
+)
